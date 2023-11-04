4cc90e0231ad8d0405114b361fa1545d703a66ca
dibinjacob <dibinjacob7794@gmail.com>
2023-11-04 18:30:29 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,17 @@
+# React Resume for Libin Mathew - Responsive and customizable CV / Resume
+
+- Light / Dark mode 🌗
+
+## Getting Started 👋
+### Built With 🛠️
+
+- [React](https://es.reactjs.org)
+- [Boxicons](https://boxicons.com)
+- [React-particles-js](https://www.npmjs.com/package/react-particles-js)
+- [React-helmet](https://github.com/nfl/react-helmet)
+
+### Running the app 🏃
+```
+# dev mode
+$ yarn dev start or npm run start
+```
---
+++ src/Schemas/Data.js
@@ -0,0 +1,127 @@
+export const Data = {
+  profile: {
+    name: "Dibin Jacob",
+    ocupation: "Cloud administrator",
+    location: "Manama, Bahrain",
+    email: "dibinjacob07071994@gmail.com",
+    telephone: "",
+    image: "images/456322.webp",
+  },
+  aboutMe: {
+    label: "Perfil",
+    description:
+      "My career objective is to pursue challenging opportunities in a dynamic and forward-thinking organization, where I can effectively apply my skills and experience to drive innovation, foster growth, and make a substantial impact on the company's success. I am dedicated to continuously enhancing my abilities and collaborating with a diverse team to achieve both personal and organizational goals, while remaining adaptable in a rapidly evolving professional landscape.",
+  },
+  skills: {
+    technicalLabel: "Technologies",
+    softLabel: "Skills",
+    technicalSkills: [
+  
+      " Terraform",
+      "AWS, Azure",
+      "Git, Bitbucket, CICD",
+      "DevOps, K8S, Jenkins",
+      "Docker, Podman",
+      "GitOps, ArgoCD",
+      "Shell Scripting",
+    ],
+    softSkills: [
+      "Teamwork",
+      "Adaptability",
+      "Conflict Resolution",
+      "Trabajo en equipo",
+      "Emotional Intelligence",
+      "Networking",
+      "Negotiation",
+      "Resilience",
+    ],
+  },
+  socialMedia: {
+    label: "SOCIAL",
+    social: [
+      {
+        label: "Dibin Jacob ",
+        name: "linkedin",
+        url: "https://www.linkedin.com/in/libinmath3w",
+        className: "bxl-linkedin-square",
+      },
+      {
+        label: "dibinjacob",
+        name: "github",
+        url: "https://github.com/libinmath3w",
+        className: "bxl-github",
+      },
+      {
+        label: "dibinjacob",
+        name: "twiiter",
+        url: "https://twitter.com/libinmathew009",
+        className: "bxl-twitter",
+      },
+    ],
+  },
+  experience: {
+    works: [
+      {
+        title: "system Engineer",
+        period: "June 2022 - Present",
+        company: "Almoayyed Computers ",
+        description: [
+          "Used jenkins to automate build and deployment that reduced human error and sped up production processes",
+          "Successfully created and maintained automated CI/CD pipelines for code deployment using jenkins while automating the deployment process, resulting in the elimination of 70% of manual work and escalation of work efficiency",
+          `Managed local deployments in kubernetes in EKS and Fargate, creating local cluster and applications deployment into containers/kubes`,
+          `Was an integral part of the team that improved and monitored cloud infrastructure on aws and jenkins.`,
+          `Implemented Terraform, Docker, kubernetes and other automation tools for infrastructure management.`,
+          `Experiance in Database (MYSQL, PLSQL, NoSQL) tools`,
+          `Experiance in Argocd and Argo rollouts for kubernetes deployments using helm chart and kustomize`,
+          `Experienced Node.js Developer proficient in server-side development, database integration, API design, security, and performance optimization. Skilled in version control, testing, and CI/CD. Strong problem-solving abilities and up-to-date with industry best practices.`,
+
+        ],
+      },
+      {
+        title: "Associate system  Engineer",
+        period: " 2021 - Mar. 2022",
+        company: "Pinmicro ",
+        description: [
+          "Developed native Android applications and frameworks using Kotlin and Java.",
+          "Defined and implemented robust app architectures and complex user interfaces.",
+          `Applied expert knowledge of Android system frameworks to solve novel problems.`,
+          `Collaborated with designers, engineers, and product managers to build great experiences.`,
+          `Experiance in server monitoring systems like prometheus and grafana.`
+        ],
+      },
+    ],
+    academic: [
+      {
+        career: "Bachelor of Computer Applications",
+        date: "2016",
+        institution: "ST. JOSEPH'S ACADEMY OF HIGHER EDUCATION AND RESEARCH, MOOLAMATTOM",
+      },
+    ],
+    proyects: [
+      {
+        name: "Hand Gesture Recognition and object Recognition using Tensorflow",
+        company: "",
+        period: "",
+        description: [
+          "The app help you to detect hand guesture recognization",
+        ],
+      },
+      {
+        name: "Automatic Door opening system IOT project",
+        company: "",
+        period: "",
+        description: [
+          "I completed automatic door opening and closing system for cats and dogs using machine learning. This project completed for a canadian university",
+        ],
+      },
+      {
+        name: "Green pool android application",
+        company: "",
+        period: "",
+        description: [
+          "An android project that developed for my university mini project.Car pooling Android application with PHP backend",
+        ],
+      },
+    ],
+  },
+};
